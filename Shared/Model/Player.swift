b9e4e12f70cba6c928a6382cf6ebae70b13d1820
linshaolong5240 <634205468@qq.com>
2020-10-15 22:29:23 +0800

    修复 播放中心歌手显示

--- Shared/Model/Player.swift
+++ Shared/Model/Player.swift
@@ -147,7 +147,7 @@ class Player: AVPlayer, ObservableObject {
         var info = [String : Any]()
         info[MPNowPlayingInfoPropertyMediaType] = MPNowPlayingInfoMediaType.audio.rawValue
         info[MPMediaItemPropertyTitle] = Store.shared.appState.playing.songDetail.name//歌名
-        info[MPMediaItemPropertyArtist] = Store.shared.appState.playing.songDetail.artists
+        info[MPMediaItemPropertyArtist] = Store.shared.appState.playing.songDetail.artists.map{$0.name}.joined(separator: " ")
         //         [info setObject:self.model.filename forKey:MPMediaItemPropertyAlbumTitle];//专辑名
         //         info[MPMediaItemPropertyAlbumArtist] = mainChannels.first?.value.soundMeta?.artist//专辑作者
         if let url = URL(string: Store.shared.appState.playing.songDetail.albumPicURL) {
