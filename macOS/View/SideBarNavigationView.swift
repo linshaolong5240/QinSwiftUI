b2666664a7503bbb9d46699f0a3aa666ad1c8f28
linshaolong5240 <634205468@qq.com>
2021-07-23 19:54:06 +0800

    update

--- macOS/View/SideBarNavigationView.swift
+++ macOS/View/SideBarNavigationView.swift
@@ -15,7 +15,7 @@ struct SideBarNavigationView: View {
     //    private var playlist: AppState.Playlist { store.appState.playlist }
     private var user: User? { store.appState.settings.loginUser }
 
-    @State private var selection: Int? = 1
+    @State private var selection: Int?
     
     var body: some View {
         NavigationView {
@@ -45,7 +45,6 @@ struct SideBarNavigationView: View {
                             .padding()
                             NavigationLink("我的云盘", destination: UserCloudView(), tag: 0, selection:  $selection)
                             NavigationLink("上传音乐", destination: CloudUploadView(), tag: 1, selection:  $selection)
-                            NavigationLink("test", destination: Text("Destination2"), tag: 2, selection:  $selection)
                         }
                     }
                 }else {
@@ -54,6 +53,9 @@ struct SideBarNavigationView: View {
             }
             .frame(minWidth: 200, idealWidth: 200, maxWidth: 200)
         }
+        .onAppear(perform: {
+            selection = 0
+        })
     }
 }
 
