37ede46067d7a6c105ddab8d0249137279dca61d
linshaolong5240 <634205468@qq.com>
2021-11-13 17:40:46 +0800

    update

--- Shared/View/NEUView/NEUButtonStyle.swift
+++ Shared/View/NEUView/NEUButtonStyle.swift
@@ -124,7 +124,7 @@ public struct NEUConvexBorderButtonStyle<S: Shape>: NEUButtonStyle {
                     let borderColors: [Color] = colorScheme == .light ? [Color( red: 245 / 255, green: 245 / 255, blue: 245 / 255), Color( red: 225 / 255, green: 230 / 255, blue: 235 / 255), Color( red: 210 / 255, green: 215 / 255, blue: 220 / 255)] : [Color( red: 33 / 255, green: 37 / 255, blue: 42 / 255), Color( red: 22 / 255, green: 24 / 255, blue: 26 / 255)]
                     let borderStrokeWidth: CGFloat = minLength / 15
 
-                    let strokeWidth: CGFloat = minLength / 25
+                    let strokeWidth: CGFloat = minLength / 20
                     let topLeftStrokeColor: Color = colorScheme == .light ? .white : .white.opacity(0.11)
 
                     shape.fill(LinearGradient(colors: backgroundColors, startPoint: .topLeading, endPoint: .bottomTrailing))
@@ -137,8 +137,7 @@ public struct NEUConvexBorderButtonStyle<S: Shape>: NEUButtonStyle {
                         shape.stroke(topLeftStrokeColor, lineWidth: strokeWidth)
                             .blur(radius: 1)
                             .offset(x: strokeWidth, y: strokeWidth)
-                            .mask(shape)
-                            .padding(borderStrokeWidth / 2)
+                            .mask(shape.padding(borderStrokeWidth / 2))
                     }
                 }
             )
