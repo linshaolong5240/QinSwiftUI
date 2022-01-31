a20341b235cbbf77b6787545b6054014836efbf8
linshaolong5240 <634205468@qq.com>
2022-01-31 12:23:47 +0800

    NCMAction

--- Shared/NeteaseCloudMusicApi/NeteaseCloudMusicApi.swift
+++ Shared/NeteaseCloudMusicApi/NeteaseCloudMusicApi.swift
@@ -15,37 +15,6 @@ public enum NCMHttpMethod: String {
     case post
 }
 
-public struct NCMEmptyParameters: Encodable {
-
-}
-
-public protocol NCMAction {
-    associatedtype Parameters: Encodable
-    associatedtype Response: Decodable
-    var method: NCMHttpMethod { get }
-    var host: String { get }
-    var uri: String { get }
-    var headers: [String: String]? { get }
-    var timeoutInterval: TimeInterval { get }
-    var parameters: Parameters? { get }
-    var responseType: Response.Type { get }
-}
-
-extension NCMAction {
-    public var method: NCMHttpMethod { .post }
-    public var headers: [String: String]? { nil }
-    public var timeoutInterval: TimeInterval { 20 }
-    public var parameters: NCMEmptyParameters? { nil }
-}
-
-extension NCMAction {
-    public var host: String { Self.defaultHost }
-    public static var defaultHost: String { "https://music.163.com" }
-    public var cloudHost: String { "https://interface.music.163.com" }
-    public var cloudUploadHost: String { "http://45.127.129.8" }
-}
-
-
 public let NCM = NeteaseCloudMusicAPI.shared
 
 public class NeteaseCloudMusicAPI {
